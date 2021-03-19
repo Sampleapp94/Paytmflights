ca957efeb889bb9fbbc525a03e7fd3830ade2cee
shiva <shiva@gmail.com>
2021-03-19 13:31:56 +0530

    Developer modified and created new file

--- Logout.java
+++ Logout.java
@@ -1,6 +1,8 @@
 // This comment will print in the document
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
+     System.out.println("Hello Java");
+     System.out.println("Welcome to Java");
+    }
+
 }  
